13ce32996a7cf389c1cfc3e48e9bbab339c75ae1
tedbeast <tedbalashov@gmail.com>
2023-01-30 14:19:12 -0500

    main method update

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -15,7 +15,7 @@ public class Application {
         System.out.println(result11);
 
         System.out.println("Currently, isEqual produces the following value for '2 is equal to 7':");
-        boolean result12 = comparisons.isEqual(5,7);
+        boolean result12 = comparisons.isEqual(2,7);
         System.out.println(result12);
 
         System.out.println("Currently, greaterThan produces the following value for '5 is greater than 3':");
@@ -23,23 +23,23 @@ public class Application {
         System.out.println(result21);
 
         System.out.println("Currently, greaterThan produces the following value for '4 is greater than 9':");
-        boolean result22 = comparisons.greaterThan(5,3);
+        boolean result22 = comparisons.greaterThan(4,9);
         System.out.println(result22);
 
         System.out.println("Currently, lessThan produces the following value for '3 is less than 6':");
-        boolean result31 = comparisons.lessThan(5,3);
+        boolean result31 = comparisons.lessThan(3,6);
         System.out.println(result31);
 
         System.out.println("Currently, lessThan produces the following value for '2 is less than 1':");
-        boolean result32 = comparisons.lessThan(5,3);
+        boolean result32 = comparisons.lessThan(2,1);
         System.out.println(result32);
 
         System.out.println("Currently, notEqualTo produces the following value for '1 is not equal to 9':");
-        boolean result41 = comparisons.notEqualTo(5,3);
+        boolean result41 = comparisons.notEqualTo(1,9);
         System.out.println(result41);
 
         System.out.println("Currently, notEqualTo produces the following value for '8 is not equal to 8':");
-        boolean result42 = comparisons.notEqualTo(5,3);
+        boolean result42 = comparisons.notEqualTo(8,8);
         System.out.println(result42);
     }
 }
